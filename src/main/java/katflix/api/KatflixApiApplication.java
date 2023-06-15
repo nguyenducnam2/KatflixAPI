176dd42c886992d51b3871cf6bc805162ab1f704
Nguyễn Đức Nam <98157595+nguyenducnam2@users.noreply.github.com>
2023-06-15 13:08:48 +0700

    set up

---
+++ src/main/java/katflix/api/KatflixApiApplication.java
@@ -0,0 +1,13 @@
+package katflix.api;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class KatflixApiApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(KatflixApiApplication.class, args);
+    }
+
+}
---
+++ src/test/java/katflix/api/KatflixApiApplicationTests.java
@@ -0,0 +1,13 @@
+package katflix.api;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class KatflixApiApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
